491383d7230332dd3af45246acc0e159bce7fd29
Akshat Mittal <itsreallyakshat@gmail.com>
2020-12-30 02:48:56 +0530

    Fixes styxlab/next-cms-ghost#6

--- pages/[...slug].tsx
+++ pages/[...slug].tsx
@@ -94,7 +94,11 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
       if (dimensions) contactPage.featureImage = { url, dimensions }
     }
   }
-  if (!post && !page && !isContactPage) throw new Error(`Expected post or page for slug: ${slug}`)
+  if (!post && !page && !isContactPage) {
+    return {
+      notFound: true,
+    }
+  }
 
   let previewPosts: GhostPostsOrPages | never[] = []
   let prevPost: GhostPostOrPage | null = null
@@ -166,6 +170,6 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
   return {
     paths,
-    fallback: enable
+    fallback: enable && "blocking"
   }
 }
