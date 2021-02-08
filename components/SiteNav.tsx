74a0fb8eaed25380b68daacf922666b9cd068307
cws <cws@localhost.localdomain>
2021-02-08 16:48:59 +0100

    minor fix

--- components/SiteNav.tsx
+++ components/SiteNav.tsx
@@ -24,7 +24,7 @@ export const SiteNav = ({ settings, className, postTitle }: SiteNavProps) => {
     addNavigation: NavItem[]
   } = {
     overwriteNavigation: customNavigation || [],
-    addNavigation: customNavigation || []
+    addNavigation: customNavigation || [],
   }
   const site = settings
   const siteUrl = settings.processEnv.siteUrl
@@ -35,10 +35,10 @@ export const SiteNav = ({ settings, className, postTitle }: SiteNavProps) => {
   const navigation = site.navigation
 
   // overwrite navigation if specified in options
-  const labels = navigation?.map(item => item.label)
+  const labels = navigation?.map((item) => item.label)
   if (labels && labels.length > 0 && config.overwriteNavigation && config.overwriteNavigation.length > 0) {
-    config.overwriteNavigation.map(item => {
-      const index = item.label && labels.indexOf(item.label) || -1
+    config.overwriteNavigation.map((item) => {
+      const index = (item.label && labels.indexOf(item.label)) || -1
       if (index > -1 && navigation && navigation[index]) {
         navigation[index].url = item.url
       }
@@ -46,16 +46,16 @@ export const SiteNav = ({ settings, className, postTitle }: SiteNavProps) => {
   }
 
   // add navigation if specified in options
-  const urls = navigation?.map(item => item.url)
+  const urls = navigation?.map((item) => item.url)
   if (config.addNavigation && config.addNavigation.length > 0) {
-    config.addNavigation.map(item => urls?.indexOf(item.url) === -1 && navigation?.push(item))
+    config.addNavigation.map((item) => urls?.indexOf(item.url) === -1 && navigation?.push(item))
   }
 
   // targetHeight is coming from style .site-nav-logo img
   const targetHeight = 21
   const calcSiteLogoWidth = (image: NextImage, targetHeight: number) => {
     const { width, height } = image.dimensions
-    return targetHeight * width / height
+    return (targetHeight * width) / height
   }
 
   return (
@@ -65,34 +65,26 @@ export const SiteNav = ({ settings, className, postTitle }: SiteNavProps) => {
           <Link href={resolve(siteUrl, '')}>
             {siteLogo && nextImages.feature ? (
               <a className="site-nav-logo">
-                <div style={{
-                  height: "${targetHeight}px",
-                  width: `${calcSiteLogoWidth(siteLogo, targetHeight)}px`
-                }}>
-                  <Image
-                    className="site-nav-logo"
-                    src={siteLogo.url}
-                    alt={title}
-                    layout="responsive"
-                    quality={nextImages.quality}
-                    {...siteLogo.dimensions}
-                  />
+                <div
+                  style={{
+                    height: '${targetHeight}px',
+                    width: `${calcSiteLogoWidth(siteLogo, targetHeight)}px`,
+                  }}
+                >
+                  <Image className="site-nav-logo" src={siteLogo.url} alt={title} layout="responsive" quality={nextImages.quality} {...siteLogo.dimensions} />
                 </div>
               </a>
-            ) : (site.logo ? (
+            ) : site.logo ? (
               <a className="site-nav-logo">
                 <img src={site.logo} alt={title} />
               </a>
             ) : (
-                <a className="site-nav-logo">
-                  {title}
-                </a>
-              ))
-            }
+              <a className="site-nav-logo">{title}</a>
+            )}
           </Link>
           <div className="site-nav-content">
             <Navigation data={navigation} />
-            {postTitle && <span className={`nav-post-title ${site.logo || `dash`}`}>{postTitle}</span>}
+            {postTitle && <span className={`nav-post-title ${site.logo ? `` : `dash`}`}>{postTitle}</span>}
           </div>
         </div>
       </div>
@@ -100,14 +92,12 @@ export const SiteNav = ({ settings, className, postTitle }: SiteNavProps) => {
         {secondaryNav ? (
           <Navigation data={site.secondary_navigation} />
         ) : (
-            <div className="social-links">
-              <SocialLinks {...{ siteUrl, site }} />
-            </div>
-          )}
-        <DarkMode {...{settings}} />
-        {memberSubscriptions && (
-          <SubscribeButton />
+          <div className="social-links">
+            <SocialLinks {...{ siteUrl, site }} />
+          </div>
         )}
+        <DarkMode {...{ settings }} />
+        {memberSubscriptions && <SubscribeButton />}
       </div>
     </nav>
   )
